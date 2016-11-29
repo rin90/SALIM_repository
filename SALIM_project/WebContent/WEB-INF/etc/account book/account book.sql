7745253f63d17a2924b6aaed26134b899b22b77a
chuhesung <cntmdal123456@gmail.com>
2016-11-29 15:12:43 +0900

    테이블

--- SALIM_project/WebContent/WEB-INF/etc/account book/account book.sql
+++ SALIM_project/WebContent/WEB-INF/etc/account book/account book.sql
@@ -1,84 +1,3 @@
-
-create table member(  --회원
- member_id varchar2(50) primary key,
- name varchar2(20) not null,
- age number not null,
- birthday date not null,  -- 생일
- password varchar2(50) not null
-);
-
-drop table member;
-
-
-create sequence seq_free  --sequence생성
-select seq_free.nextval from dual  -- 조회 
-drop sequence seq_free  --삭제
-
-create table free_board( --자유게시판
- no number primary key,
- title varchar2(300) not null,
- content varchar2(4000) not null,
- file_root varchar2(4000),  --파일 경로
- click	number not null,  --조회수
- good number not null,  --좋아요
- category varchar2(20) not null,
- member_id varchar2(50) constraint fre_mem_fk references member not null--inline방식
-);
-
-drop table free_board;
-
-
-
-create sequence seq_tip--sequence생성
-select seq_tip.nextval from dual  -- 조회 
-drop sequence seq_tip--삭제
-
-create table tip_board(  --tip게시판
-	no number primary key,
-	title varchar2(3000) not null,
-	content varchar2(4000) not null,
-	file_root varchar2(4000),  -- 파일 경로
-	category varchar2(50) not null,
-	click number not null,   -- 조회수 
-	good number not null,   -- 좋아요
-	member_id varchar2(50) constraint tip_mem_fk references member not null--inline방식
-);
-
-drop table tip_board;
-
-
-create sequence seq_com--sequence생성
-select seq_com.nextval from dual  -- 조회 
-drop sequence seq_com--삭제
-
-create table comments(  --댓글
-	id number primary key,
-	content varchar2(400) not null,
-	register_time date not null,
-	font_no number not null,  -- 어떤 글인지 번호
-	category varchar2(10) not null,    
-	member_id varchar2(50) constraint com_mem_fk references member not null
-);
-
-drop table comments;
-
-
-
-create sequence seq_qaa--sequence생성
-select seq_qaa.nextval from dual  -- 조회 
-drop sequence seq_qaa  --삭제
-
-create table question_and_answer(  -- Q&A 게시판
-	id number primary key,
-	category varchar2(50) not null, -- 주제  ( 예, 로그인,홈페이지 ,게시판 등등
-	content varchar2(4000) not null,
-	title varchar2(300) not null,
-	member_id varchar2(50) constraint qaa_mem_fk references member not null
-);
-
-drop table question_and_answer;
-
-
 create sequence seq_nts  --sequence생성
 select seq_nts.nextval from dual  -- 조회 
 drop sequence seq_nts--삭제
@@ -166,36 +85,6 @@ schedule_date Date not null,  --날짜
 cotent Varchar2(300) not null  --상세내용
 );
 
-drop table schedule;
-
-create sequence seq_col--sequence생성
-select seq_sch.nextval from dual  -- 조회 
-drop sequence seq_col--삭제
-
-create table collection(--그룹
-collection_id Number not null,--그룹id
-collection_name varchar2(50) not null,--그룹이름
-collection_intro varchar2(300)--그룹소개
-
-
-drop table collection;
-);
-
-create sequence seq_mco--sequence생성
-select seq_sch.nextval from dual  -- 조회 
-drop sequence seq_mco--삭제
-
-create table membercollection(--회원그룹
-member_id varchar2(50) not null,--회원id
-collection_id Number not null,--그룹id
-grant_id varchar2(300) not null--권한자id
-
-drop table membercollection;
-
-);
-
-
-
 create sequence seq_goa--sequence생성
 select seq_sch.nextval from dual  -- 조회 
 drop sequence seq_goa--삭제
@@ -221,10 +110,3 @@ code varchar2(300)not null--코드
 drop table code;
 
 );
-
-
-
-
-
-
-
---
+++ SALIM_project/WebContent/WEB-INF/etc/board/board.sql
@@ -0,0 +1,62 @@
+create sequence seq_free  --sequence생성
+select seq_free.nextval from dual  -- 조회 
+drop sequence seq_free  --삭제
+
+create table free_board( --자유게시판
+ no number primary key,
+ title varchar2(300) not null,
+ content varchar2(4000) not null,
+ file_root varchar2(4000),  --파일 경로
+ click	number not null,  --조회수
+ good number not null,  --좋아요
+ category varchar2(20) not null,
+ member_id varchar2(50) constraint fre_mem_fk references member not null--inline방식
+);
+
+drop table free_board;
+
+create sequence seq_tip--sequence생성
+select seq_tip.nextval from dual  -- 조회 
+drop sequence seq_tip--삭제
+
+create table tip_board(  --tip게시판
+	no number primary key,
+	title varchar2(3000) not null,
+	content varchar2(4000) not null,
+	file_root varchar2(4000),  -- 파일 경로
+	category varchar2(50) not null,
+	click number not null,   -- 조회수 
+	good number not null,   -- 좋아요
+	member_id varchar2(50) constraint tip_mem_fk references member not null--inline방식
+);
+
+drop table tip_board;
+
+create sequence seq_com--sequence생성
+select seq_com.nextval from dual  -- 조회 
+drop sequence seq_com--삭제
+
+create table comments(  --댓글
+	id number primary key,
+	content varchar2(400) not null,
+	register_time date not null,
+	font_no number not null,  -- 어떤 글인지 번호
+	category varchar2(10) not null,    
+	member_id varchar2(50) constraint com_mem_fk references member not null
+);
+
+drop table comments;
+
+create sequence seq_qaa--sequence생성
+select seq_qaa.nextval from dual  -- 조회 
+drop sequence seq_qaa  --삭제
+
+create table question_and_answer(  -- Q&A 게시판
+	id number primary key,
+	category varchar2(50) not null, -- 주제  ( 예, 로그인,홈페이지 ,게시판 등등
+	content varchar2(4000) not null,
+	title varchar2(300) not null,
+	member_id varchar2(50) constraint qaa_mem_fk references member not null
+);
+
+drop table question_and_answer;
