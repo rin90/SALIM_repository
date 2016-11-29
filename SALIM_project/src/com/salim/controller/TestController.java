e37424c7946c290d7591431d33ff99942182d1d3
leejeachan <dlwocks1234@naver.com>
2016-11-29 17:49:37 +0900

    cxz

--- SALIM_project/src/com/salim/controller/TestController.java
+++ SALIM_project/src/com/salim/controller/TestController.java
@@ -1,22 +1,5 @@
 package com.salim.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.salim.service.impl.TestServiceImpl;
-
-@Controller
-@RequestMapping("/test")
 public class TestController {
-	@Autowired
-	private TestServiceImpl service;
-	
-	@RequestMapping("/time.do")
-	public String timeForNow(ModelMap map){
-		String time = service.time();
-		map.addAttribute("time", time);
-		return "/main.jsp";
-	}
+
 }
