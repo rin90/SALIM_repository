4e8925039ce1bb3eaa37973999c98d8c0384714d
leejeachan <dlwocks1234@naver.com>
2016-11-29 19:20:05 +0900

    수정

--- SALIM_project/WebContent/WEB-INF/etc/member/member.sql
+++ SALIM_project/WebContent/WEB-INF/etc/member/member.sql
@@ -26,8 +26,8 @@ drop table collection;
 
 
 create table member_collection(--회원그룹
-member_id varchar2(50) constraint mco_col_fk references collection,
-collection_id Number constraint mco_mem_fk references member,
+member_id varchar2(50) constraint mco_col_fk references member,
+collection_id Number constraint mco_mem_fk references collection,
 grant_id varchar2(300) not null,--권한자id
 constraint mc_pk primary key(member_id,collection_id)
 );
