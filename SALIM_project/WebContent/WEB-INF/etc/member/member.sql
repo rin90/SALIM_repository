54ca8f2df9226fbf20b42255815ea0fb26e37252
KimTaeho <xogh0207@naver.com>
2016-12-07 16:20:17 +0900

    카드&은행 sql

--- SALIM_project/WebContent/WEB-INF/etc/member/member.sql
+++ SALIM_project/WebContent/WEB-INF/etc/member/member.sql
@@ -55,31 +55,3 @@ constraint mc_pk primary key(member_id,collection_id)
 
 
 drop table member_collection;
-
------------------------------------------------------------------------------------------------------ 11.30 타이허
-create table card(
-	member_id varchar2(50),
-	card_id Number primary key,
-	card_Type varchar2(50) not null,
-	card_description varchar2(100) not null
-);
-
-drop table card;
-
-create sequence card_id
-select card_id.nextval from dual
-drop sequence card_id
-
-create table bank(
-	member_id varchar2(50), 
-	bank_id Number primary key,
-	bank_Type varchar2(50) not null,
-	bank_description varchar2(100) not null
-);
-
-drop table bank;
-
-create sequence bank_id
-select bank_id.nextval from dual --조회
-drop sequence bank_id
-
