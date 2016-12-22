3b64f825957c80be80204d39fd0c8b1174576fcf
leejeachan <dlwocks1234@naver.com>
2016-12-22 14:18:56 +0900

    a

---
+++ SALIM_project/WebContent/WEB-INF/etc/board/boardDummyData.sql
@@ -0,0 +1,159 @@
+drop sequence seq_dummy_f;--자유
+
+
+drop sequence seq_dummy_t; --tip
+
+drop sequence seq_dummy_fc; --자유 댓글
+
+drop sequence seq_dummy_tc; --tip 댓글
+--더미 데이터에 사용할 sequence 자유게시판
+create sequence seq_dummy_f; 
+
+
+--자유게시판
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+insert into FREE_BOARD values(seq_dummy_f.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'2016-01,'||seq_dummy_f.nextval,'test');
+
+
+----------------------------------------------------------------------------------------------------------
+
+--더미 데이터에 사용할 sequence 자유게시판
+create sequence seq_dummy_t; 
+
+
+
+--tip게시판
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'생활','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'생활','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'생활','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'생활','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'생활','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'생활','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'생활','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'생활','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'생활','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'생활','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'생활','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'생활','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'저축','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'저축','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'저축','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'생활','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'생활','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'생활','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'수입','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'생활','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'수입','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'생활','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'수입','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'생활','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'지출','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'생활','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'지출','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'생활','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'생활','2016-01,'||seq_dummy_t.nextval,'test');
+insert into TIP_BOARD values(seq_dummy_t.nextval,'SALIM','바이바이','images719TZLL3.jpg',0,'생활','2016-01,'||seq_dummy_t.nextval,'test');
+
+--------------------------------------------------------------------------------------------------------
+
+
+--더미 데이터에 사용할 sequence 자유게시판 댓글
+create sequence seq_dummy_fc; 
+
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+insert into FREE_COMMENTS values(seq_dummy_fc.nextval,'간다~~~.','test',seq_dummy_fc.nextval,seq_dummy_fc.nextval,0,'2016-01,'||seq_dummy_fc.nextval);
+
+
+
+
+
+------------------------------------------------------------------------------------------------------------
+--더미 데이터에 사용할 sequence tip게시판 댓글
+create sequence seq_dummy_tc; 
+select seq_dummy_tc.nextval from dual
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
+insert into tip_COMMENTS values(seq_dummy_tc.nextval,'간다~~~.','test',seq_dummy_tc.nextval,seq_dummy_tc.nextval,0,'2016-01,'||seq_dummy_tc.nextval);
